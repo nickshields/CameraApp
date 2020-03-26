139e9a9b0fcd4e089be05986f20ae1c379c8b65d
Nick Shields <Nick_Shields@ultimatesoftware.com>
2020-03-25 20:18:45 -0400

    Initial Commit

---
+++ CameraApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CameraApp
+//
+//  Created by Nick Shields on 2020-03-25.
+//  Copyright © 2020 Nick Shields. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
