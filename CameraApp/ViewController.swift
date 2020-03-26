84038cb3a8bf5ce2ed130cb47cc0d9e40ab0479b
Nick Shields <Nick_Shields@ultimatesoftware.com>
2020-03-25 20:41:24 -0400

    Part one of iOS AVFoundation

--- CameraApp/ViewController.swift
+++ CameraApp/ViewController.swift
@@ -7,14 +7,49 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    let session = AVCaptureSession()
+    var camera : AVCaptureDevice?
+    var cameraPreviewLayer : AVCaptureVideoPreviewLayer?
+    var cameraCaptureOutput : AVCapturePhotoOutput?
+    
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        initializeCaptureSession()
         // Do any additional setup after loading the view.
     }
 
+    func initializeCaptureSession(){
+        session.sessionPreset = AVCaptureSession.Preset.high
+        camera = AVCaptureDevice.default(for: .video)
+        
+        do{
+            let cameraCaptureInput = try AVCaptureDeviceInput(device: camera!)
+            cameraCaptureOutput = AVCapturePhotoOutput()
+            
+            session.addInput(cameraCaptureInput)
+            session.addOutput(cameraCaptureOutput!)
+            
+        }catch{
+            print(error.localizedDescription)
+        }
+        
+        cameraPreviewLayer = AVCaptureVideoPreviewLayer(session: session)
+        cameraPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        cameraPreviewLayer?.frame = view.bounds
+        cameraPreviewLayer?.connection?.videoOrientation = AVCaptureVideoOrientation.portrait
+        
+        view.layer.insertSublayer(cameraPreviewLayer!, at: 0)
+        
+        session.startRunning()
+        
+        
+    }
 
 }
 
